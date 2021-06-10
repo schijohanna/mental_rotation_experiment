8bd6a0a1eb954464158cbfdd10ab33bde950e3ae
schijohanna <83468749+schijohanna@users.noreply.github.com>
2021-06-10 15:15:53 +0200

    Add files via upload

---
+++ 04_trials.js
@@ -0,0 +1,25 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+    key_press_trials: [
+    {
+        question: "The two shown figures are the same just in a different orientation.",
+        picture: 'path/to/picture.jpg',
+        key1: 'f',
+        key2: 'j',
+        f: 'correct',
+        j: 'incorrect',
+        expected: 'correct'
+    },
+    {
+        question: "The two shown figures are the same just in a different orientation.",
+        picture: 'path/to/picture.jpg',
+        key1: 'f',
+        key2: 'j',
+        f: 'correct',
+        j: 'incorrect',
+        expected: 'incorrect'
+    }
+  ];
+};
